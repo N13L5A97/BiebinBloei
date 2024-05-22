9b9a9bd3916f986fa204d4cc3daccfc72dbcade4
Niels Aling <niels.aling@hva.nl>
2024-05-22 15:04:14 +0200

    refactor: Update homepageData to include new cardData

--- scripts/homepageData.js
+++ scripts/homepageData.js
@@ -22,5 +22,17 @@ export const cardData = [
             "url": "#linkie",
             "text": "Lees meer"
         }
+    },
+    {
+        "title": "hoi 2",
+        "content": "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempore doloribus beatae dignissimos reprehenderit obcaecati, totam aut vitae consequatur saepe ad doloremque rerum inventore, aliquid, fuga eum. Dignissimos, unde voluptas!</p> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tempore doloribus beatae dignissimos reprehenderit obcaecati, totam aut vitae consequatur saepe ad doloremque rerum inventore, aliquid, fuga eum. Dignissimos, unde voluptas!</p>",
+        "image": {
+            "src": "https://via.placeholder.com/150",
+            "alt": "placeholder"
+        },
+        "button": {
+            "url": "#linkie",
+            "text": "Lees meer"
+        }
     }
 ]
