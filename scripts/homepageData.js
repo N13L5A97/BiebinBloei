740faf404a0b802337621fcc81b99e62c63acb89
Niels Aling <niels.aling@hva.nl>
2024-05-23 14:41:24 +0200

    updated card styling

--- scripts/homepageData.js
+++ scripts/homepageData.js
@@ -1,6 +1,6 @@
 export const cardData = [
     {
-        "title": "Onze Stekjes Bieb",
+        "title": "Onze <br> Stekjes <br> Bieb",
         "content": "<p>Bij Buurtcampus Oost kun je de stekjeskast bezoeken om stekjes te ruilen, je kunt ook op onze website inzien welke stekjes beschikbaar zijn. Dit is een geweldige manier voor plantenliefhebbers om met elkaar in contact te komen en planten te delen.</p><p>Het maakt het gemakkelijk om je favoriete stekjes te vinden en te ruilen met anderen in de buurt, zonder gedoe. Kom langs bij Buurtcampus Oost en ontmoet mede-plantenliefhebbers!</p>",
         "image": {
             "src": "https://via.placeholder.com/150",
@@ -12,7 +12,7 @@ export const cardData = [
         }
     },
     {
-        "title": "Onze Zaden Bieb",
+        "title": "Onze <br> Zaden <br> Bieb",
         "content": "<p>Bij Buurtcampus Oost kun je de zadenkast bezoeken om zaadjes te kunnen ruilen, je kunt ook op onze website inzien welke stekjes beschikbaar zijn. Dit is een geweldige manier voor plantenliefhebbers om met elkaar in contact te komen en planten te delen.</p><p>Het maakt het gemakkelijk om je favoriete stekjes te vinden en te ruilen met anderen in de buurt, zonder gedoe. Kom langs bij Buurtcampus Oost en ontmoet mede-plantenliefhebbers!</p>",
         "image": {
             "src": "https://via.placeholder.com/150",
@@ -24,7 +24,7 @@ export const cardData = [
         }
     },
     {
-        "title": "Onze Gevel Tuinen",
+        "title": "Onze <br> Gevel <br> Tuinen",
         "content": "<p>Bij Buurtcampus Oost kan je deelnemen aan de geveltuin projecten. Het geveltuinproject is een geweldige manier om je steentje bij te dragen aan een mooier, groener en socialer Amsterdam. Of je nu een ervaren tuinier bent of nog nooit met een plant hebt gewerkt, er is altijd een plek voor je in ons team.</p> <p>We leren je alles wat je moet weten en zorgen voor de benodigde materialen. Meld je aan vandaag nog!</p>",
         "image": {
             "src": "https://via.placeholder.com/150",
