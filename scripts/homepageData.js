4fe241964b0865cd24894e840131e4a0a3f816bc
Niels Aling <niels.aling@hva.nl>
2024-05-23 15:41:36 +0200

    finished cards css

--- scripts/homepageData.js
+++ scripts/homepageData.js
@@ -3,36 +3,39 @@ export const cardData = [
         "title": "Onze <br> Stekjes <br> Bieb",
         "content": "<p>Bij Buurtcampus Oost kun je de stekjeskast bezoeken om stekjes te ruilen, je kunt ook op onze website inzien welke stekjes beschikbaar zijn. Dit is een geweldige manier voor plantenliefhebbers om met elkaar in contact te komen en planten te delen.</p><p>Het maakt het gemakkelijk om je favoriete stekjes te vinden en te ruilen met anderen in de buurt, zonder gedoe. Kom langs bij Buurtcampus Oost en ontmoet mede-plantenliefhebbers!</p>",
         "image": {
-            "src": "https://via.placeholder.com/150",
-            "alt": "placeholder"
+            "src": "../images/plant1.png",
+            "alt": "planten potjes"
         },
         "button": {
             "url": "/stekjes",
-            "text": "Bekijk de Bieb"
+            "text": "Bekijk de Bieb",
+            "aria": "Ga naar de stekjes bieb"
         }
     },
     {
         "title": "Onze <br> Zaden <br> Bieb",
         "content": "<p>Bij Buurtcampus Oost kun je de zadenkast bezoeken om zaadjes te kunnen ruilen, je kunt ook op onze website inzien welke stekjes beschikbaar zijn. Dit is een geweldige manier voor plantenliefhebbers om met elkaar in contact te komen en planten te delen.</p><p>Het maakt het gemakkelijk om je favoriete stekjes te vinden en te ruilen met anderen in de buurt, zonder gedoe. Kom langs bij Buurtcampus Oost en ontmoet mede-plantenliefhebbers!</p>",
         "image": {
-            "src": "https://via.placeholder.com/150",
-            "alt": "placeholder"
+            "src": "../images/zaad1.png",
+            "alt": "zaadjes",
         },
         "button": {
             "url": "/zaden",
-            "text": "Bekijk de Bieb"
+            "text": "Bekijk de Bieb",
+            "aria": "Ga naar de zaadjes bieb"
         }
     },
     {
         "title": "Onze <br> Gevel <br> Tuinen",
         "content": "<p>Bij Buurtcampus Oost kan je deelnemen aan de geveltuin projecten. Het geveltuinproject is een geweldige manier om je steentje bij te dragen aan een mooier, groener en socialer Amsterdam. Of je nu een ervaren tuinier bent of nog nooit met een plant hebt gewerkt, er is altijd een plek voor je in ons team.</p> <p>We leren je alles wat je moet weten en zorgen voor de benodigde materialen. Meld je aan vandaag nog!</p>",
         "image": {
-            "src": "https://via.placeholder.com/150",
-            "alt": "placeholder"
+            "src": "../images/geveltuin1.png",
+            "alt": "geveltuin"
         },
         "button": {
             "url": "/geveltuin",
-            "text": "Lees meer"
+            "text": "Lees meer",
+            "aria": "lees meer over onze geveltuinen"
         }
     }
 ]
