8ee6b089adffebf3d0b282ccff486f1f9504daf6
Niels Aling <niels.aling@hva.nl>
2024-06-04 11:12:11 +0200

    fix image slider loop

--- src/js/image-slider.js
+++ src/js/image-slider.js
@@ -11,18 +11,18 @@ document.addEventListener("DOMContentLoaded", function () {
 		currentSlide++;
 
 		// if the current slide is equal to the total slides, go back to the first slide
-		if (currentSlide === totalSlides) {
-			slider.style.setProperty("transition-duration", "0s");
-			currentSlide = 0;
-			let translateX = 0;
-			slider.style.setProperty("--translateX", translateX);
+		if (currentSlide === totalSlides ) {
+      slider.style.setProperty("transition-duration", "0s");
+      translateX = 0;
+      currentSlide = 0;
+      slider.style.setProperty("--translateX", translateX);
 
       setTimeout(() => {
         currentSlide++;
+        translateX = (100 / totalSlides) * currentSlide;
         slider.style.setProperty("transition-duration", "1s");
-        slider.style.setProperty("--translateX", -100);
+        slider.style.setProperty("--translateX", translateX);
       }, 10);
-
 
 			// if the current slide is greater or equal to 0, move to the next slide
 		} else if (currentSlide < totalSlides) {
