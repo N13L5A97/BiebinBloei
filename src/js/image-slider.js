4a95a65dcfb471552862faa0ba89ca1fcf34ccb8
Niels Aling <niels.aling@hva.nl>
2024-06-05 10:11:59 +0200

    chore: Update image slider to use img tags instead of background images

--- src/js/image-slider.js
+++ src/js/image-slider.js
@@ -1,32 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const slider = document.getElementById("imageSlider");
-	const slides = Array.from(document.querySelectorAll(".slide"));
+	const slides = slider.querySelectorAll(".slide");
 	const totalSlides = slides.length;
 	let currentSlide = 0;
 	let translateX = 0;
 
 	setInterval(() => {
 		console.log("current slide:", currentSlide);
-		// after 3 seconds go to next slide
+		//go to next slide
 		currentSlide++;
 
 		// if the current slide is equal to the total slides, go back to the first slide
-		if (currentSlide === totalSlides ) {
-      slider.style.setProperty("transition-duration", "0s");
-      translateX = 0;
-      currentSlide = 0;
-	  
-	  setTimeout(() => {
-      	slider.style.setProperty("--translateX", translateX);
-		
-		  setTimeout(() => {
-			currentSlide++;
-			translateX = (100 / totalSlides) * currentSlide;
-			slider.style.setProperty("transition-duration", "1s");
-			slider.style.setProperty("--translateX", translateX);
-		  }, 10);
-	  }, 10);
+		if (currentSlide === totalSlides) {
+			slider.style.setProperty("transition-duration", "0s");
+			translateX = 0;
+			currentSlide = 0;
 
+			setTimeout(() => {
+				slider.style.setProperty("--translateX", translateX);
+
+				setTimeout(() => {
+					currentSlide++;
+					translateX = (100 / totalSlides) * currentSlide;
+					slider.style.setProperty("transition-duration", "1s");
+					slider.style.setProperty("--translateX", translateX);
+				}, 10);
+			}, 10);
 
 			// if the current slide is greater or equal to 0, move to the next slide
 		} else if (currentSlide < totalSlides) {
@@ -34,5 +33,6 @@ document.addEventListener("DOMContentLoaded", function () {
 			slider.style.setProperty("transition-duration", "1s");
 			slider.style.setProperty("--translateX", translateX);
 		}
-	}, 3000);
+		//after how many seconds the slide should change 
+	}, 4500);
 });
